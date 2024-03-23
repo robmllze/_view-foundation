72499066aacd4223789b58cb830f06f261685c3b
robmllze <86869208+robmllze@users.noreply.github.com>
2024-03-23 13:47:59 +1100

    Update

--- lib/src/widgets/screen/_screen_view.dart
+++ lib/src/widgets/screen/_screen_view.dart
@@ -14,9 +14,7 @@ part of 'screen.dart';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-abstract class ScreenView<
-    T1 extends Screen,
-    T2 extends ModelScreenConfiguration,
+abstract class ScreenView<T1 extends Screen, T2 extends ModelScreenConfiguration,
     T3 extends ScreenController<T2>> extends State<T1> {
   //
   //
@@ -42,8 +40,7 @@ abstract class ScreenView<
     if (key != null) {
       if (_controllerCache[key] == null) {
         _controllerCache[key] = _ControllerCache(
-          (this.widget.createController(this.widget, this)..initController())
-              as T3,
+          (this.widget.createController(this.widget, this)..initController()) as T3,
           controllerTimeout != null
               ? Debouncer(
                   delay: controllerTimeout,
@@ -58,8 +55,7 @@ abstract class ScreenView<
       this.c = _controllerCache[key]?.controller as T3;
       _controllerCache[key]?.debouncer?.cancel();
     } else {
-      this.c = (this.widget.createController(this.widget, this)
-        ..initController()) as T3;
+      this.c = (this.widget.createController(this.widget, this)..initController()) as T3;
     }
     super.initState();
   }
@@ -93,26 +89,57 @@ abstract class ScreenView<
   /// ```
   @mustCallSuper
   Widget layout(Widget body) {
-    final navigationControls =
-        this.widget.configuration?.navigationControlsWidget;
+    final navigationControls = this.widget.configuration?.navigationControlsWidget;
     final makeup = letAs<ScreenMakeup>(this.widget.configuration?.makeup);
-    return Column(
-      children: [
-        Expanded(
-          child: Container(
-            color: makeup?.backgroundColor,
-            child: body,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SizedBox(
+          width: constraints.maxWidth,
+          height: constraints.maxHeight,
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  width: constraints.maxWidth,
+                  color: makeup?.backgroundColor,
+                  child: body,
+                ),
+              ),
+              if (navigationControls != null) navigationControls,
+            ],
           ),
-        ),
-        if (navigationControls != null) navigationControls,
-      ],
+        );
+      },
     );
   }
+
+  //
+  //
+  //
+
+  Widget wideLayout(Widget body) {
+    return Center(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: constraints.maxHeight / MIN_MOBILE_ASPECT_RATIO,
+              maxHeight: double.infinity,
+            ),
+            child: this.layout(body),
+          );
+        },
+      ),
+    );
+  }
+
+  //
+  //
+  //
 
   Widget mobileLayout(Widget body) => this.narrowLayout(body);
   Widget horizontalMobileLayout(Widget body) => this.wideLayout(body);
   Widget narrowLayout(Widget body) => this.layout(body);
-  Widget wideLayout(Widget body) => this.layout(body);
 
   //
   //
@@ -141,8 +168,7 @@ abstract class ScreenView<
           case AppLayout.MOBILE:
             return this.mobileLayout(this.mobileBody(context));
           case AppLayout.MOBILE_HORIZONTAL:
-            return this
-                .horizontalMobileLayout(this.horizontalMobileBody(context));
+            return this.horizontalMobileLayout(this.horizontalMobileBody(context));
           case AppLayout.NARROW:
             return this.narrowLayout(this.narrowBody(context));
           case AppLayout.WIDE:
