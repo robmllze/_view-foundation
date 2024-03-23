555e317dcfc80e2743603a871860e3d04a6f663a
robmllze <86869208+robmllze@users.noreply.github.com>
2024-03-23 12:04:24 +1100

    Update

--- lib/src/types/accessibility/app_locale_type/app_locale_type.dart
+++ lib/src/types/accessibility/app_locale_type/app_locale_type.dart
@@ -25,10 +25,7 @@ enum AppLocaleType with AppLocaleEnumMixin {
   // NB: File names are case sensitive on most Linux based systems like
   // Firebase Hosting. Make sure to use the correct case for your file paths.
   // As a rule of thumb, always use lowercase file paths.
-  en_us('en_us'),
-  af_za('af_za'),
-  de_de('de_de'),
-  es_us('es_us');
+  en_us('en_us');
 
   //
   //
