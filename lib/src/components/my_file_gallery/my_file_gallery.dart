9cf9bdd7f1a16f3f78c37981f18e488359d93f67
robmllze <86869208+robmllze@users.noreply.github.com>
2024-07-07 16:22:12 +1000

    Update

--- lib/src/components/my_file_gallery/my_file_gallery.dart
+++ lib/src/components/my_file_gallery/my_file_gallery.dart
@@ -56,8 +56,8 @@ class _State extends State<MyFileGallery> {
   late final _orderedFiles = List.of(this.widget.files)
     ..sort((a, b) {
       final now = DateTime.now();
-      final da = a.createdReg?.at ?? now;
-      final db = b.createdReg?.at ?? now;
+      final da = a.createdReg?.registeredAt ?? now;
+      final db = b.createdReg?.registeredAt ?? now;
       final c = da.compareTo(db);
       return c;
     });
